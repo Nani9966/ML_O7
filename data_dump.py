297c26ac6d825a505166f295d5bfc0b15b6c0e72
Chinna <chinnareddy2106@gmail.com>
2022-12-08 19:44:49 +0530

    Files insertion in git hub

---
+++ data_dump.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import pymongo
+import json
+
+###  mongo db localhost url to connect python to mongo db
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
+if __name__ == "__main__":
+    df=pd.read_csv("/config/workspace/aps_failure_training_set1.csv")
+    print(f"rows and columns:{df.shape}")
+
+    json_record=list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    
---
+++ git_codes.py
@@ -0,0 +1,55 @@
+
+       git remote -v
+       git remote remove origin ----------------> origin in name of git  
+      git remote add origin https://github.com/Nani9966/ML_O7.git ----> git link
+      git log
+       git  remote -v 
+       git push origin main  ##connecing the hit hub by credicentials
+       git pull origin main   ##call file s from server   
+       here need to  save comment after push so two aare one we should delete 
+       comment of previous in git hub  or else delet comments which are at git log
+
+       ls -a  to khows hidden folder which all commits are saved
+       cd .git/
+       .gits ls
+       cd logs/
+
+
+       ###git log delete 
+       ls -a   # to show hidden folder
+       cd .git/
+       ls
+       cd logs
+       ls 
+       cat HEAD  ---> TO SEE LOGS
+       cd ..   >>coming out of git directory
+       cd ..
+
+###################repeated  above steps 
+        ls 
+        cd .git/
+        ls 
+        cd logs/
+        ls 
+        cat HEAD
+        cd refs/
+        ls
+        cd remotes/
+        ls    >>> origin here this is importtanfttgtttttttttttttt
+        cd origin/
+        ls 
+        cat main
+        git reset 
+
+
+
+#############to reset the comments  type this code in termonal
+git reset --soft 6afd
+
+
+
+
+
+
+
+
