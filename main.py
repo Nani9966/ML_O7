9359b23beb0b7376dca8f32a956f31e2cf463b81
chinna <chinna@gmail.com>
2022-12-11 00:13:52 +0530

    add

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
-import sys
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.entity.config_entity import DataIngectionConfig
+
+# from sensor.components.data_ingestion import DataIngestion
 
 # def test_logger_and_exception():
 #      try:
@@ -13,11 +18,25 @@ import sys
 #           raise SensorException(e, sys)
 
 
+# if __name__=="__main__":
+#      try:
+#       #      test_logger_and_exception()
+#             get_collection_as_dataframe(database_name="aps",collection_name="sensor")
+#      except Exception as e:
+#           print(e)
+
+
 if __name__=="__main__":
      try:
-           test_logger_and_exception()
+            training_pipeline_config=config_entity.TrainingPipelineConfig()
+            data_ingestion_config=DataIngectionConfig(training_pipeline_config=training_pipeline_config)
+            print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)
+
+
+          
+
 
 
 
---
+++ sensor/__init__.py
@@ -0,0 +1,6 @@
+
+
+from dotenv import load_dotenv
+print(f"Loading environment varible from .env file")
+load_dotenv()
+
