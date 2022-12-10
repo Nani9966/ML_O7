946db2c754bcabc7f11e9e38fa9d484fa13857eb
chinna <chinnareddy2106@gmail.com>
2022-12-10 13:38:01 +0530

    updte_main

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,5 +1,5 @@
 import logging
-impodt os
+import os
 from datetime import datetime
 import os
 
@@ -18,6 +18,6 @@ LOG_FILE_PATH=os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s- %(levelname)s-%(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 
 )
